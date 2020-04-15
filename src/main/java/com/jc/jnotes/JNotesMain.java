9e8dc9b88d8cfc3a37e4d5ee8f5e50938a189049
Joy <joy.chakravarty84@gmail.com>
2020-04-15 19:13:05 +0200

    added text notification

---
+++ src/main/java/com/jc/jnotes/JNotesMain.java
@@ -0,0 +1,16 @@
+package com.jc.jnotes;
+
+/**
+ * 
+ * Main class which doesn't extend javafx specific class.
+ * 
+ * @author Joy C
+ *
+ */
+public class JNotesMain {
+
+    public static void main(String[] args) {
+        JNotesApplication.main(args);
+    }
+
+}
